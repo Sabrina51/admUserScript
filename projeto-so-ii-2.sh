9d0013f57b4268497d770795e11050c1b390066b
Sabrina Silva (She/Her) <76228258+Sabrina51@users.noreply.github.com>
2022-06-03 22:18:08 -0300

    Add files via upload

--- projeto-so-ii-2.sh
+++ projeto-so-ii-2.sh
@@ -17,7 +17,7 @@ do
     echo ""
     echo "2 - [Configuração de expiração de contas]"
     echo ""
-    echo "3 - [Desativar contas com 30 dias e nunca foi usada]"
+    echo "3 - [Desativar contas criadas a 30 dias e que nunca foram usadas]"
     echo ""
     echo "4 - [Exit]"
     echo ""
@@ -34,47 +34,101 @@ do
         echo "Digite a quantidade de usuários permanentes que deseja criar"
         read quant_usuario_perm
         echo ""
-        echo "Digite o nome padão dos usuários permanentes"
-        read nome_usuario_perm
+
+        echo "Digite a quantidade de usuários temporários que deseja criar"
+        read quant_usuario_temp
         echo ""
-        # echo "Digite a quantidade de usuários temporários que deseja criar"
-        # read quant_usuario_temp
-        # echo ""
-        # echo "Digite o nome padão dos usuários temporários"
-        # read nome_usuario_temp
-        # echo ""
 
-        for count in $(seq 1 $quant_usuario_perm); do
-            usuario_definido=$nome_usuario_perm$count
-            echo -e "\033[01;32mCriando usuário: [$usuario_definido]...\033[01;37m"
-            sleep 1
-            useradd $usuario_definido -p $(openssl passwd -1 $usuario_definido)
-            id $usuario_definido
-            echo ""
-
-            echo -e "\033[01;32mConfigurando senha do usuário: [$usuario_definido]...\033[01;37m"
-            sleep 1
-            passwd --expire $usuario_definido
-            chage -M 60 $usuario_definido
-            chage -l $usuario_definido
-            echo ""
-
-            echo -e "\033[01;32mConfigurando expiração de conta do usuário: [$usuario_definido]...\033[01;37m"
-            sleep 1
-
-            echo ""
-
-            echo -e "\033[01;36mUsuario $usuario_definido finalizado.\033[01;37m"
-
+        if test $quant_usuario_perm -gt 0
+        then
+            echo -e "\033[01;33mCentral de criação de usuários permanentes\033[01;37m"
             echo "-----------------------------------------------------------------------"
+            echo "Digite o nome padão dos usuários permanentes"
+            read nome_usuario_perm
+            sleep 5
             echo ""
-        done
+
+            for count in $(seq 1 $quant_usuario_perm); do
+                usuario_definido=$nome_usuario_perm$count
+
+                echo -e "\033[01;32mCriando usuário: [$usuario_definido]...\033[01;37m"
+                sleep 1
+
+                useradd $usuario_definido -p $(openssl passwd -1 $usuario_definido)
+                id $usuario_definido
+                echo ""
+
+                echo -e "\033[01;32mConfigurando senha do usuário: [$usuario_definido]...\033[01;37m"
+                sleep 1
+                
+                passwd --expire $usuario_definido
+                chage -M 60 $usuario_definido
+                chage -l $usuario_definido
+                echo ""
+
+                echo -e "\033[01;32mConfigurando expiração de conta do usuário: [$usuario_definido]...\033[01;37m"
+                sleep 1
+
+                echo ""
+
+                echo -e "\033[01;36mUsuario $usuario_definido finalizado.\033[01;37m"
+
+                echo "-----------------------------------------------------------------------"
+                echo ""
+            done
+        fi
+
+        if test $quant_usuario_temp -gt 0
+        then
+            echo -e "\033[01;33mCentral de criação de usuários temporários\033[01;37m"
+            echo "-----------------------------------------------------------------------"
+            echo "Digite o nome padão dos usuários temporários"
+            read nome_usuario_perm
+            sleep 5
+            echo ""
+
+            for count in $(seq 1 $quant_usuario_temp); do
+                usuario_definido=$quant_usuario_temp$count
+
+                echo -e "\033[01;32mCriando usuário: [$usuario_definido]...\033[01;37m"
+                sleep 1
+
+                useradd $usuario_definido -p $(openssl passwd -1 $usuario_definido)
+                id $usuario_definido
+                echo ""
+
+                echo -e "\033[01;32mConfigurando senha do usuário: [$usuario_definido]...\033[01;37m"
+                sleep 1
+                
+                passwd --expire $usuario_definido
+                chage -M 60 $usuario_definido
+                chage -l $usuario_definido
+                echo ""
+
+                echo -e "\033[01;32mConfigurando expiração de conta do usuário: [$usuario_definido]...\033[01;37m"
+                sleep 1
+                echo "Data de expiração para usuários temporários é de 6 meses após a data de criação da conta"
+                hoje=$(date +%Y/%m/%d)
+                
+                chage -E 183 $hoje $nome_usuario
+                echo ""
+                chage -l $nome_usuario
+                echo ""
+
+                echo -e "\033[01;36mUsuario $usuario_definido finalizado.\033[01;37m"
+
+                echo "-----------------------------------------------------------------------"
+                echo ""
+            done
+        fi
+
     echo "-----------------------------------------------------------------------"
     ;;
     2)
         echo "Digite o nome do usuário que deseja definir uma senha:"
-        read userNome
-        passwd $userNome
+        hoje=$(date +%Y/%m/%d)
+        echo $hoje
+        echo $(date +%Y/%m/%d)
         echo ""
         echo -e "\033[01;33mDigite qualquer tecla para voltar para o menu principal!\033[01;37m"
         read
